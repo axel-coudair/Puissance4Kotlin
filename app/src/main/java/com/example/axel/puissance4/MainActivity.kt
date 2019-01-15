9818665eee0de5366cad864b0719405fac262ee1
Axel Coudair <axel.coudair.dev@gmail.com>
2019-01-15 10:49:16 +0100

    :bug: loading view in main

--- app/src/main/java/com/example/axel/puissance4/MainActivity.kt
+++ app/src/main/java/com/example/axel/puissance4/MainActivity.kt
@@ -7,24 +7,23 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    val button = findViewById<Button>(R.id.btnPlay)
-    val firstPlayerName = findViewById<EditText>(R.id.editTextPlayer1)
-    val secondPlayerName = findViewById<EditText>(R.id.editTextPlayer2)
-    val errorMessageTextView = findViewById<TextView>(R.id.textViewError)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnPlay)
 
         button.setOnClickListener {
-            onPlayClick(it)
+            onPlayClick()
         }
     }
 
-    fun onPlayClick(view: View) {
+    fun onPlayClick() {
+        val errorMessageTextView = findViewById<TextView>(R.id.textViewError)
+    val firstPlayerName = findViewById<EditText>(R.id.editTextPlayer1)
+    val secondPlayerName = findViewById<EditText>(R.id.editTextPlayer2)
         var firstPlayerNameText = firstPlayerName.text.toString().trim()
         var secondPlayerNameText = secondPlayerName.text.toString().trim()
         if (!firstPlayerNameText.isNullOrBlank() && !secondPlayerNameText.isNullOrBlank()) {
