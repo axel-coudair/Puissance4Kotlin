f4f98e0cadf0e29f9cb3ea2c902f44fcfd3c5b4c
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-02-03 20:27:46 +0100

    feature switch player | tokenColor

---
+++ app/src/main/java/com/example/axel/puissance4/model/TokenColor.kt
@@ -0,0 +1,10 @@
+package com.example.axel.puissance4.model
+
+import com.example.axel.puissance4.R
+
+class TokenColor {
+    companion object {
+        const val YELLOW = R.drawable.yellow
+        const val RED = R.drawable.rouge
+    }
+}
--- app/src/main/java/com/example/axel/puissance4/presentation/P4View.kt
+++ app/src/main/java/com/example/axel/puissance4/presentation/P4View.kt
@@ -3,7 +3,7 @@ package com.example.axel.puissance4.presentation
 import com.example.axel.puissance4.model.Player
 
 interface P4View {
-    fun setNameLabel(player1: Player, player2: Player)
-    fun getPlayer1Name(): String
-    fun getPlayer2Name(): String
+
+    fun setTurnText(player: Player)
+    fun setNamesText(player1: Player, player2: Player)
 }
